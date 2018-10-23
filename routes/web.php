74675224dea0d01ca1c0eea198dfcf8550b9b94f
廖文忠 <wcliao1962@gmail.com>
2018-10-24 00:27:53 +0800

    練習7-1, 2: Route to HomeController@index

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return redirect('welcome');
-});
+Route::get('/', ['as'=>'home.index', 'uses'=>'HomeController@index']);
 
 Route::get('welcome', function () {
     return view('welcome');
