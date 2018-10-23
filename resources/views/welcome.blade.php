842aac9badbe054f093242768627746d8b0ee9ea
廖文忠 <wcliao1962@gmail.com>
2018-10-24 00:06:14 +0800

    練習4-1, 2: 為route命名

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <a href="{{ route('hello.index') }}">Hello</a>
                 </div>
 
                 <div class="links">
