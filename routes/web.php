ad5f5edff9659c8cc07cc6d77f830c8102a09d4c
廖文忠 <wcliao1962@gmail.com>
2018-10-25 22:04:55 +0800

    練習5 更正

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,13 @@ Route::get('say/{name?}', ['as'=>'hello.index', function ($name='Everybody') {
     return 'Hello, '.$name;
 }]);
 
+Route::get('dashboard', function () {
+    return 'dashboard';
+});
+
 Route::group(['prefix'=>'admin'], function () {
     Route::get('dashboard', function () {
-        return 'dashboard';
+        return 'admin dashboard';
     });
 
 });
